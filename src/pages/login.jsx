b59ecd9fcea09f72d2c376bc92f965f539b429d2
HaaniAshraf <155221611+HaaniAshraf@users.noreply.github.com>
2024-04-23 13:07:14 +0530

    initial commit

---
+++ src/pages/login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+
+const login = () => {
+  return (
+    <div className='common'>
+        <div>
+            <img src={Logo} alt="" />
+            <h2>CineMagic</h2>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default login
